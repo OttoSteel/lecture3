ad5e6cb3e55dae4ff2dc26cdffdef9563485a0e9
OttoSteel <polshinski@gmail.com>
2018-11-13 00:52:30 +0200

    Add EventHandler & DriverManager

--- src/main/java/support/MainTest.java
+++ src/main/java/support/MainTest.java
@@ -1,13 +1,9 @@
 package support;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
